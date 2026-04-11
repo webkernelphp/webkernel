cf5c0eda1e964e58ab9024a3080d9652d9f65f88
emyassine <info@numerimondes.com>
2026-04-11 03:11:06 +0100

    WIP

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
-    App\Providers\Filament\InstallerPanelProvider::class,
     Webkernel\Aptitudes\System\Providers\InstallerPanelProvider::class,
 ];
