95cecff7f493b9382da7e1f531bfcc9c3906bcec
emyassine <info@numerimondes.com>
2026-04-24 04:08:38 +0100

    feat: Add navigation groups with MenuItem to SystemPanelProvider
    Restore static navigation groups for System, Infrastructure, Marketplace,
    and Maintenance sections using Filament's MenuItem API.

--- bootstrap/webkernel/src/BackOffice/System/Providers/SystemPanelProvider.php
+++ bootstrap/webkernel/src/BackOffice/System/Providers/SystemPanelProvider.php
@@ -23,8 +23,11 @@ use Webkernel\BackOffice\System\Presentation\Resources\DependencyManager\Pages\N
 use Webkernel\BackOffice\System\Presentation\Resources\DependencyManager\FilamentDependencyManagerServiceProvider;
 use Webkernel\BackOffice\System\Models\WebkernelBackgroundTask;
 use Webkernel\BackOffice\System\Presentation\Resources\BackgroundTasks\BackgroundTasksResource;
+use Webkernel\BackOffice\System\Presentation\Resources\WebkernelSettings\WebkernelSettingResource;
 use Filament\Support\Enums\Width;
 use Filament\Support\Colors\Color;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\MenuItem;
 
 final class SystemPanelProvider extends PanelProvider
 {
@@ -48,6 +51,64 @@ final class SystemPanelProvider extends PanelProvider
             ->topbar()
             ->spa()
             ->globalSearch()
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('System')
+                    ->icon('layout-dashboard')
+                    ->items([
+                        MenuItem::make('Settings')
+                            ->icon('cog')
+                            ->url(fn () => WebkernelSettingResource::getUrl('index')),
+                        MenuItem::make('Logging')
+                            ->icon('document-text'),
+                        MenuItem::make('Security')
+                            ->icon('lock-closed'),
+                    ]),
+
+                NavigationGroup::make()
+                    ->label('Infrastructure')
+                    ->icon('database')
+                    ->collapsed()
+                    ->items([
+                        MenuItem::make('Users & Access')
+                            ->icon('users'),
+                        MenuItem::make('Storage')
+                            ->icon('document'),
+                        MenuItem::make('Database')
+                            ->icon('server'),
+                        MenuItem::make('Performance')
+                            ->icon('chart-bar')
+                    ]),
+
+                NavigationGroup::make()
+                    ->label('Marketplace')
+                    ->icon('shopping-bag')
+                    ->collapsed()
+                    ->items([
+                        MenuItem::make('Modules')
+                            ->icon('puzzle'),
+                        MenuItem::make('Integrations')
+                            ->icon('link')
+                    ]),
+
+                NavigationGroup::make()
+                    ->label('Maintenance')
+                    ->icon('wrench')
+                    ->collapsed()
+                    ->items([
+                        MenuItem::make('Composer')
+                            ->icon('code-bracket')
+                            ->url(fn () => DependencyManagerPage::getUrl()),
+                        MenuItem::make('NPM')
+                            ->icon('code-bracket')
+                            ->url(fn () => NpmDependencyManagerPage::getUrl()),
+                        MenuItem::make('Background Tasks')
+                            ->icon('play')
+                            ->url(fn () => BackgroundTasksResource::getUrl('index')),
+                        MenuItem::make('Monitoring')
+                            ->icon('signal')
+                    ]),
+            ])
             ->sidebarCollapsibleOnDesktop()
             ->maxContentWidth('screen-xxl')
             ->colors([
